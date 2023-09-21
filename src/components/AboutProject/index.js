25169852f973885e8d8397eb54035e0d1ecdcb43
Ivan Degtiarenko <ia.degtyarenko@gmail.com>
2023-09-21 17:27:07 +0100

    feat: Rest of the AboutProject component

--- src/components/AboutProject/index.js
+++ src/components/AboutProject/index.js
@@ -1,20 +1,14 @@
 import Section from "../Section";
+import SectionTitle from "../SectionTitle";
 
 import "./AboutProject.css";
-
-function Factoid({ name, description }) {
-  return (
-    <div className="about-project__factoid">
-      <p className="about-project__factoid-name">{name}</p>
-      <p className="about-project__factoid-description">{description}</p>
-    </div>
-  );
-}
+import "./Factoid.css";
+import "./Timeline.css";
 
 export default function AboutProject() {
   return (
     <Section className="about-project">
-      <h2 className="about-project__title">О проекте</h2>
+      <SectionTitle>О проекте</SectionTitle>
       <div className="about-project__factoids">
         <Factoid
           name="Дипломный проект включал 5 этапов"
@@ -25,6 +19,31 @@ export default function AboutProject() {
           description="У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься."
         />
       </div>
+      <Timeline />
     </Section>
+  );
+}
+
+function Factoid({ name, description }) {
+  return (
+    <div className="factoid">
+      <p className="factoid__name">{name}</p>
+      <p className="factoid__description">{description}</p>
+    </div>
+  );
+}
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      <div className="timeline__segment">
+        <p className="timeline__bar timeline__bar_highlighted">1 неделя</p>
+        <p className="timeline__caption">Back-end</p>
+      </div>
+      <div className="timeline__segment">
+        <p className="timeline__bar">4 недели</p>
+        <p className="timeline__caption">Front-end</p>
+      </div>
+    </div>
   );
 }
---
+++ src/components/SectionTitle/index.js
@@ -0,0 +1,5 @@
+import "./SectionTitle.css";
+
+export default function SectionTitle({ children }) {
+  return <h2 className="section-title">{children}</h2>;
+}
