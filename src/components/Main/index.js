7af749b8606a155434b90b75b1632a630d3c520a
Ivan Degtiarenko <ia.degtyarenko@gmail.com>
2023-09-23 18:08:57 +0100

    feat: Sticky footer

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,9 +4,11 @@ import Techs from "../Techs";
 import AboutMe from "../AboutMe";
 import Portfolio from "../Portfolio";
 
+import "./Main.css";
+
 export default function Main() {
   return (
-    <main>
+    <main className="main">
       <Promo />
       <AboutProject />
       <Techs />
