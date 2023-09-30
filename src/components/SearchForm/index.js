ed23d98a7c714fd2cdaca803c8341076aa698762
Ivan Degtiarenko <ia.degtyarenko@gmail.com>
2023-09-30 18:27:45 +0100

    fix: Add semantic section tag

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -7,21 +7,23 @@ import IconButton from "ui/IconButton";
 
 export default function SearchForm() {
   return (
-    <Section tagName="form" className="search-form section_mobile-margins_m">
-      <div className="search-form__field">
-        <FormField
-          id="query"
-          className="search-form__input"
-          type="search"
-          placeholder="Фильм"
-        />
-        <IconButton
-          className="search-form__button"
-          ariaLabel="Искать фильмы"
-          type="submit"
-        />
-      </div>
-      <Switch className="search-form__switch" label="Короткометражки" />
+    <Section className="search-form section_mobile-margins_m">
+      <form className="search-form__form">
+        <div className="search-form__field">
+          <FormField
+            id="query"
+            className="search-form__input"
+            type="search"
+            placeholder="Фильм"
+          />
+          <IconButton
+            className="search-form__button"
+            ariaLabel="Искать фильмы"
+            type="submit"
+          />
+        </div>
+        <Switch className="search-form__switch" label="Короткометражки" />
+      </form>
     </Section>
   );
 }
