95742f58428ae714c891c5f266a8218c0b1ff42a
Ivan Degtiarenko <ia.degtyarenko@gmail.com>
2023-09-30 18:43:05 +0100

    feat: PaginationControl block

--- src/modules/MoviesExplorer/index.js
+++ src/modules/MoviesExplorer/index.js
@@ -1,11 +1,13 @@
 import MovieCardsGrid from "components/MovieCardsGrid";
 import SearchForm from "components/SearchForm";
+import PaginationControl from "components/PaginationControl";
 
 export default function MoviesExplorer({ savedMovies = false }) {
   return (
     <>
       <SearchForm />
       <MovieCardsGrid savedMovies={savedMovies} />
+      <PaginationControl />
     </>
   );
 }
