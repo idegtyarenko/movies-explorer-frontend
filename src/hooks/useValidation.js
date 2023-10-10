7bc35616fae5db85bb606c4c5daa0c7de70b08d6
Ivan Degtiarenko <ia.degtyarenko@gmail.com>
2023-10-10 20:42:20 +0100

    feature: Form validation support

--- src/hooks/useValidation.js
+++ src/hooks/useValidation.js
@@ -2,14 +2,19 @@ import { useState } from "react";
 
 export default function useValidation(initialValues) {
   const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(false);
 
   function handleChange(e) {
-    const { name, value, type, checked } = e.target;
+    const input = e.target;
+    const { name, value, type, checked, validationMessage } = input;
 
     const typeDependentValue = type === "checkbox" ? checked : value;
 
     setValues({ ...values, [name]: typeDependentValue });
+    setErrors({ ...errors, [name]: validationMessage });
+    setIsValid(input.closest("form").checkValidity());
   }
 
-  return { values, handleChange };
+  return { values, errors, isValid, handleChange };
 }
