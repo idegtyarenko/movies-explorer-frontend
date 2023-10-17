d4b972ce778cc5db7d5ff2f1857387812cd2ab89
Ivan Degtiarenko <ia.degtyarenko@gmail.com>
2023-10-17 23:47:39 +0100

    fix: Email without second level domain considered valid

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,6 +16,7 @@ export const EMAIL_FIELD = new FieldDescription("email", "E-mail", "email", {
   required: true,
   maxLength: 128,
   placeholder: "Например, vasilisa@yandex.ru",
+  pattern: "[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-z]{2,4}$",
 });
 
 export const PASSWORD_FIELD = new FieldDescription(
